cb08c1cdd47c212b5cb459a9d0a8c25d1022db81
denpayanara <88641815+denpayanara@users.noreply.github.com>
2023-10-21 08:46:37 +0900

    Update main.py

--- main.py
+++ main.py
@@ -13,9 +13,11 @@ ctx.options |= 0x4
 with request.urlopen(url, context=ctx) as r:
     urls = re.findall(r'"(/assets/json/area-project-\d{6}-\d{6}.json)"', r.read().decode('utf-8'))
 
-with open('data/url.text', mode='r') as f:
+with open('data/url.text', mode='r', newline='') as f:
     before_url = f.read()
 
 print(urls[0] == before_url)
+
+print(urls[0], before_url)
 
 print(type(urls[0]), type(before_url))
