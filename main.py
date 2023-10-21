034e5f024838c5122490299626cca9564174e3b1
denpayanara <88641815+denpayanara@users.noreply.github.com>
2023-10-21 15:08:02 +0900

    Update main.py

--- main.py
+++ main.py
@@ -170,17 +170,17 @@ for index, row in df_nara.iterrows():
 
     # print(row['addr'])
 
-    merge_data = pd.merge(gdf_area_0[gdf_area_0['addr'].str.contains(row['addr'])].reset_index(drop=True), df_nara, left_on='CITY_NAME', right_on='City')
+    merge_data = pd.merge(gdf_area_0[gdf_area_0['addr'].str.contains(row['addr'])].reset_index(drop=True), df_nara, on='addr')
 
     gdf_area_1 = pd.concat([gdf_area_1, merge_data])
 
 # 同一地域の融合
-# 念の為IDで
+# 4Gと5Gは同一のID
 
 gdf_dissolve = gdf_area_1.dissolve(by=['ID', 'Type', 'Date'], as_index=False)
 
 # gdf_dissolve.columns
 
-gdf_area_2 = gdf_dissolve[['flag', 'ID', 'Date', 'Type', 'PostalCode', 'Prefecture', 'City', 'addr_y', 'geometry']].rename(columns={'addr_y': 'addr'}).sort_values('flag')
+gdf_area_2 = gdf_dissolve[['ID', 'Date', 'Type', 'PostalCode', 'Prefecture', 'City', 'addr', 'geometry']]
 
 gdf_area_2.to_file('data/開局エリア.geojson', driver='GeoJSON', index=False)
